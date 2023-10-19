f8ee8e76a0af8589bd4041a7faa0eedb52c2c3f8
Amine Meftah <amine.gamer222@gmail.com>
2023-10-19 23:08:36 +0100

    first operatinal version

--- main.c
+++ main.c
@@ -2,49 +2,39 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 /**
 *main-Entry Point
 *Return: 1 always
 */
+struct stack_s *stack2;
 int main(int argc, char **argv)
 {
-    unsigned long int line = 0;
-    unsigned int line_number = 1;
-    char *line_buffer = NULL;
-    int buffer_lenght = 0;
-    FILE *file = NULL;
-    struct stack_s *stack = NULL;
+	unsigned int line_number = 1;
+	char line_buffer[4090];
+	FILE *file = NULL;
 
-    stack = malloc(sizeof(int));
-    if (stack == NULL)
-    {
-        dprintf(2,"Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    if (argc != 2)
-    {
-        dprintf(2,"USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
-    /*read from the specified file passed by the user*/
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        dprintf(2,"Error: Can't open file %s\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-        
-
-    while ((buffer_lenght = getline(&line_buffer, &line, file)) != -1)
-    {
-        main_interpreter(line_buffer, buffer_lenght, line_number, stack);
-        line_number++;
-    }
-
-    fclose(file);
-    if (line_buffer)
-        free(line_buffer);
-    free(stack);
-
-    return (EXIT_SUCCESS);
+	stack2 = NULL;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	/*read from the specified file passed by the user*/
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(line_buffer, sizeof(line_buffer), file) != NULL)
+	{
+		main_interpreter(line_buffer, strlen(line_buffer), line_number, file);
+		line_number++;
+	}
+	fclose(file);
+	if (stack2 != NULL)
+		free_stack();
+	return (EXIT_SUCCESS);
 }
